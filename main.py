1b132f121f8589c10d09e1042f24d1ea924ea5b3
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2023-08-20 18:12:13 +0530

    using portable brave

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import eel
+
+eel.init('web')
+
+eel.start(
+        'index.html',
+        mode='brave'  # Or whichever name u created at mode=='<some_name>' from before
+) 
+# eel.start('index.html')
